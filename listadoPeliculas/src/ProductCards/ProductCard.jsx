ea7a30d9e61af351d9eb98e90a0349907fdbec72
Matias Gerardi <113802541+MatiGerardi@users.noreply.github.com>
2025-03-26 14:22:04 -0300

    refactor of: ProductCard, ProductList and Movie (from search api). Add: orderby to the getAll methon of serverModel

--- listadoPeliculas/src/ProductCards/ProductCard.jsx
+++ listadoPeliculas/src/ProductCards/ProductCard.jsx
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
+// import { deleteMovie, addMovie } from "../apiDB";
+// import { useContext } from "react";
+// import { MoviesContext } from "../context/MoviesContext";
 import "./ProductCard.css";
 
 function ProductCard({ movie, onButtonClick, buttonText }) {
-
   return (
     <div className="product-card">
       <div className="product-card-content">
-        <div className="product-image">
         <div className="product-image">
           <img src={movie.poster} alt={movie.title} className="product-image" />
         </div>
@@ -29,19 +30,15 @@ function ProductCard({ movie, onButtonClick, buttonText }) {
           <hr className="divider-line" />
           <div className="product-rating">
             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="product-star-icon">
-            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="product-star-icon">
               <path d="M0 0h24v24H0z" fill="none"></path>
               <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
             </svg>
             <span className="product-rating-text">{movie.rating ? movie.rating : "N/A"}</span>
-            <span className="product-rating-text">{movie.rating ? movie.rating : "N/A"}</span>
           </div>
-          <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
           <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
             <p>Hover for more ...</p>
           </div>
         </div>
-
 
         <div className="product-card-actions">
           <div className="product-description">
@@ -49,11 +46,6 @@ function ProductCard({ movie, onButtonClick, buttonText }) {
             <br />
             {movie.plot}
           </div>
-          {onButtonClick && buttonText && (
-            <button onClick={onButtonClick ? () => onButtonClick(movie) : undefined}>
-              {buttonText ? buttonText : "Undifined"}
-            </button>
-            )}
           {onButtonClick && buttonText && (
             <button onClick={onButtonClick ? () => onButtonClick(movie) : undefined}>
               {buttonText ? buttonText : "Undifined"}
@@ -78,8 +70,6 @@ ProductCard.propTypes = {
     runtime: PropTypes.string.isRequired,
     genre: PropTypes.string.isRequired,
   }).isRequired,
-  onButtonClick: PropTypes.func,
-  buttonText: PropTypes.string,
   onButtonClick: PropTypes.func,
   buttonText: PropTypes.string,
 };
